3b3d35c2da4de5945e1a2b9d618262506f6b287a
owenpacc07 <owenpacc@gmail.com>
2025-03-06 16:41:18 -0500

    Updated file / folder structure
    more aligned with the example weve been working on in class

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue!</h1>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style>
+@import './assets/styles/main.css';
+</style>
---
+++ client/src/components/Exercises.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1>Exercises</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Exercises'
+  }
+  </script>
+  
+  <style scoped>
+  /* optional styles here */
+  </style>  
