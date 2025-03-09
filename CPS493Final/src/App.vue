de09e4acb94d17c235a427b47bf720bdf85b2edd
owenpacc07 <owenpacc@gmail.com>
2025-03-09 14:14:03 -0400

    Added main outline for project
    blank files and folders added

--- CPS493Final/src/App.vue
+++ CPS493Final/src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const isLoggedIn = ref(false) // This will be managed by your auth store
 </script>
 
 <template>
+  <header>
+    <nav class="container">
+      <RouterLink to="/">Home</RouterLink>
+      <template v-if="!isLoggedIn">
+        <RouterLink to="/login">Login</RouterLink>
+      </template>
+      <template v-else>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
+        <RouterLink to="/activities">Activities</RouterLink>
+        <RouterLink to="/friends">Friends</RouterLink>
+      </template>
+    </nav>
+  </header>
+
   <RouterView />
 </template>
 
@@ -14,10 +31,26 @@ import { RouterView } from 'vue-router'
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
+} 
 
 .container {
   max-width: 1100px;
   margin: 0 auto;
+}
+
+nav {
+  padding: 1rem 0;
+  display: flex;
+  gap: 1rem;
+
+  a {
+    text-decoration: none;
+    color: #333;
+    font-weight: 500;
+
+    &.router-link-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ CPS493Final/src/data/users.js
@@ -0,0 +1,18 @@
+export const users = [
+    {
+      id: 1,
+      username: 'admin',
+      password: 'admin123',
+      name: 'Admin User',
+      role: 'admin',
+      friends: []
+    },
+    {
+      id: 2,
+      username: 'user',
+      password: 'user123',
+      name: 'Regular User',
+      role: 'user',
+      friends: []
+    }
+  ]
