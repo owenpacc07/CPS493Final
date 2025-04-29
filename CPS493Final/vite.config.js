5e27a167af48f698219b8ad3625cac25068ac142
owenpacc07 <owenpacc@gmail.com>
2025-04-29 14:30:29 -0400

    fixes

--- CPS493Final/vite.config.js
+++ CPS493Final/vite.config.js
@@ -1,25 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/CPS493Final/' : '/',
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
     }
   },
   build: {
@@ -28,20 +18,11 @@ export default defineConfig({
     target: 'esnext',
     minify: 'esbuild',
     chunkSizeWarningLimit: 1000,
-    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: {
           'vendor': ['vue', 'vue-router', 'pinia']
         }
-      }
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
       }
     }
   }
