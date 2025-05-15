760ae70d76a55add1691edaf62c8156ff1ccc328
owenpacc07 <owenpacc@gmail.com>
2025-05-15 13:03:22 -0400

    Revert "Added auto textbox features"
    This reverts commit 7ba0f3bb1d6ae53e5e6ee1de236f770e2d183b47.

--- CPS493Final/src/main.js
+++ CPS493Final/src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Oruga from '@oruga-ui/oruga'
-import '@oruga-ui/oruga/dist/oruga.css'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
 
+console.log('Initializing app...')
 app.use(pinia)
 app.use(router)
-app.use(Oruga)
 
 app.mount('#app')
+console.log('App mounted')
