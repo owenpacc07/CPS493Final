a3fceb4e9ee80472b6d1f9174a3414958d815ff4
owenpacc07 <owenpacc@gmail.com>
2025-05-15 13:37:30 -0400

    added oruga to project

--- CPS493Final/src/main.js
+++ CPS493Final/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Oruga from '@oruga-ui/oruga'
+import '@oruga-ui/oruga/dist/oruga.css'
 import App from './App.vue'
 import router from './router'
 
@@ -9,6 +11,7 @@ const pinia = createPinia()
 console.log('Initializing app...')
 app.use(pinia)
 app.use(router)
+app.use(Oruga)
 
 app.mount('#app')
 console.log('App mounted')
