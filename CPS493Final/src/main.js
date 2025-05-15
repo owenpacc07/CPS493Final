a141931543f531d7fb22f34d32460ce123e28cce
owenpacc07 <owenpacc@gmail.com>
2025-05-15 13:50:15 -0400

    Revert "added oruga to project"
    This reverts commit a3fceb4e9ee80472b6d1f9174a3414958d815ff4.

--- CPS493Final/src/main.js
+++ CPS493Final/src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Oruga from '@oruga-ui/oruga'
-import '@oruga-ui/oruga/dist/oruga.css'
 import App from './App.vue'
 import router from './router'
 
@@ -11,7 +9,6 @@ const pinia = createPinia()
 console.log('Initializing app...')
 app.use(pinia)
 app.use(router)
-app.use(Oruga)
 
 app.mount('#app')
 console.log('App mounted')
